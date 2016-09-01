33ef4c2c743e5fd5d2582f1b96e493a70e920a44
Amit Ranan <ranan.amit@gmail.com>
2016-09-01 16:04:50 -0700

    small style fixes

--- client/components/AboutSite/AboutSite.jsx
+++ client/components/AboutSite/AboutSite.jsx
@@ -8,7 +8,7 @@ var AboutSite = React.createClass({
                 <h3>About this site</h3>
                 <ul className="about-site-container">
                     <li className="about-site-item">
-                        <h3>This responsive site has been designed, developed, and maintained by me. It's built in <span className="about-site-highlight">React</span> using <span className="about-site-highlight">Sass</span> as a CSS extention language, and <span className="about-site-highlight">Webpack</span> as a module bundler.
+                        <h3>This responsive site has been designed, developed, and maintained by me. It's built in <span className="about-site-highlight">React</span> using <span className="about-site-highlight">Sass</span> as a CSS extension language, and <span className="about-site-highlight">Webpack</span> as a module bundler.
                         </h3>
                     </li>
                 </ul>
