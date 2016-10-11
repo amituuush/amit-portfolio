dd704dbc2c02f3d6c5efbabc8f9146dcdc043b00
Amit Ranan <ranan.amit@gmail.com>
2016-10-11 12:59:35 -0700

    change Lyst image

--- client/components/Projects/Projects.jsx
+++ client/components/Projects/Projects.jsx
@@ -26,18 +26,18 @@ var Projects = React.createClass({
                         </div>
                         <div className="projects-item-content item-content-gomocha-ba">
                             <h3>GoMocha Business Admin (WIP)</h3>
-                            <p>GoMocha Business Admin is the other component that receives the orders placed on GoMocha Consumer. This would be the coffee shop interface in a real world setting. GoMocha as a whole is a monolithic app. The consumer and business admin apps both have their own front ends and share the same back end and database.</p>
+                            <p>GoMocha Business Admin is the component that receives the orders placed on GoMocha Consumer. This would be the coffee shop interface in a real world setting. GoMocha as a whole is a monolithic app. The consumer and business admin apps both have their own front ends and share the same back end and database.</p>
                             <p><span>Technologies:</span> React, Node.js, Express, MongoDB, Mongoose, Google Maps API, Heroku, Travis CI, Webpack, SASS</p>
                         </div>
                     </div>
                     <div className="projects-item">
                         <div className="projects-item-image">
-                            <img src="img/todo-list.png" />
+                            <img src="img/lyst.png" />
                         </div>
                         <div className="projects-item-content item-content-todo-list">
                             <h3>Lyst</h3>
-                            <p>Lyst is a to do list application I built in React and Redux. I'm currently in the process of adding a backend in Node.js, database using MongoDB and Mongoose, and user authentication using Passport.</p>
-                            <p><span>Technologies: </span>React, Redux, Node.js, Express, MongoDB, Mongoose, Passport, Travis CI, LESS</p>
+                        <p>Lyst is a multiple to do list application with priority, due date, and filter settings. The front end is built in React and Redux, and uses LESS as a css pre-processor. The back end is build in Node, Express, and Mongoose, and uses a MongoDB database. Authentication middleware is handled with Passport.</p>
+                    <p><span>Technologies: </span>React, Redux, Node.js, Express, MongoDB, Mongoose, Passport, Heroku, Travis CI, LESS</p>
                             <a href="https://lyst-app.herokuapp.com/" target="_blank"><button>Demo</button></a>
                             <a name="about-me"></a>
                             <a href="https://github.com/amituuush/lyst" target="_blank"><button>Repo</button></a>
