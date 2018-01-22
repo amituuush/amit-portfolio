43f3ff1d5b519f5485e9f483f06e17fb8aef578b
Amit Ranan <ranan.amit@gmail.com>
2018-01-21 20:08:38 -0800

    updated resume

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -30,7 +30,7 @@ var App = React.createClass({
                     menuShow={this.state.menuShow} />
                 <Cover />
                 <Projects />
-                <AboutMe />
+                {/* <AboutMe /> */}
                 <Contact />
                 <AboutSite />
                 <Footer />
