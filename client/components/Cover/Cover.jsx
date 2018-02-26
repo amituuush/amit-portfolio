489ba9c76498a045cef5bd8c59faf3210de4e0ee
Amit Ranan <ranan.amit@gmail.com>
2018-02-25 18:58:51 -0800

    updated one liner, resume

--- client/components/Cover/Cover.jsx
+++ client/components/Cover/Cover.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import sass from './cover.scss'
 
 var Cover = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <section className="cover-section">
                 <a name="home"></a>
@@ -12,8 +12,8 @@ var Cover = React.createClass({
                 <div className="cover-right">
                     <h1>Amit Ranan</h1>
                     <a name="portfolio"></a>
-                    <h2>Passionate and motivated SWE at <a href="https://www.therealreal.com/" target="_blank">The RealReal</a> with 3 years experience building web apps in React/Redux/Node/Elixir/Phoenix and developing web pages.</h2>
-                    <a href="./img/amit-ranan-resume.pdf" target="_blank"><button>Resume</button></a>
+                    <h2>Software Engineer at <a href="https://www.therealreal.com/" target="_blank">The RealReal</a> with 3 years experience building web apps in React/Redux/Node/Elixir/Phoenix. <a href="500 Startups">500 Startups</a> Batch 13.</h2>
+                    <a href="./img/amit_ranan_resume.pdf" target="_blank"><button>Resume</button></a>
                 </div>
             </section>
         )
--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import sass from './nav.scss'
 
 var Nav = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <nav className="">
-                <div onClick={() => {this.props.handleMenuToggle()}} className="menu-bars">
-                    <i className={this.props.menuShow ? 'fa fa-times fa-2x' : 'fa fa-bars fa-2x' } aria-hidden="true"></i>
+                <div onClick={() => { this.props.handleMenuToggle() }} className="menu-bars">
+                    <i className={this.props.menuShow ? 'fa fa-times fa-2x' : 'fa fa-bars fa-2x'} aria-hidden="true"></i>
                 </div>
                 <img src="img/amit-logo.png" />
                 <ul className={this.props.menuShow ? 'menu-show' : 'menu-hide'}>
-                    <a href="#home" onClick={() => {this.props.handleMenuToggle()}}><li>Home</li></a>
-                    <a href="#portfolio" onClick={() => {this.props.handleMenuToggle()}}><li>Portfolio</li></a>
-                    <a href="#about-me" onClick={() => {this.props.handleMenuToggle()}}><li>About Me</li></a>
-                    <a href="#contact" onClick={() => {this.props.handleMenuToggle()}}><li>Contact</li></a>
+                    <a href="#home" onClick={() => { this.props.handleMenuToggle() }}><li>Home</li></a>
+                    <a href="#portfolio" onClick={() => { this.props.handleMenuToggle() }}><li>Portfolio</li></a>
+                    <a href="#about-me" onClick={() => { this.props.handleMenuToggle() }}><li>About Me</li></a>
+                    <a href="#contact" onClick={() => { this.props.handleMenuToggle() }}><li>Contact</li></a>
                     <a href="https://www.linkedin.com/in/amitranan" target="_blank">
                         <li>LinkedIn</li>
                     </a>
-                    <a href="./img/amit-ranan-resume.pdf" target="_blank" onClick={() => {this.props.handleMenuToggle()}}><li>Resume</li></a>
+                    <a href="./img/amit_ranan_resume.pdf" target="_blank" onClick={() => { this.props.handleMenuToggle() }}><li>Resume</li></a>
                 </ul>
             </nav>
         )
